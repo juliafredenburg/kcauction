a07bb55d75c68ca61379b90428b99e6eab71ae5b
Patrick <pjpweaver@gmail.com>
2015-08-04 21:57:25 -0400

    add permutations of errors

--- kcauction.py
+++ kcauction.py
@@ -1,4 +1,5 @@
 import csv
+import itertools
 
 # read copy and pasted lines from pdf into a list
 
@@ -14,24 +15,38 @@ with open('DailyRecord01-03.txt', 'r') as records:
     if line != '\n':
       no_blank_lines.append(line)
 
-# create a new sublist each time a new "K2014" is read
+# create a new sublist each time a new "K2014" is read, add the K2014 number, the price, and th 00-000 number to the sublist.
 
   records = []
+  zero_characters = ['0', 'O', 'Q', 'o', 'P']
+  possible_errors = []
+  possible_errors_with_dashes = []
+  possible_errors_as_permutations = itertools.product(zero_characters, repeat=5)
+
+  j = 0
+  for error in possible_errors_as_permutations:
+    possible_errors.append('')
+    k = 0
+    for character in error:
+      possible_errors[j] += character
+      k += 1
+      if k == 2:
+        possible_errors[j] += '-'
+    j += 1
+
+
   i = -1
   for line in no_blank_lines:
     if 'K2014' in line:
-      records.append([line])
+      price_start = line.find('$')
+      k_end = price_start - 1
+      records.append([line[0:k_end]])
       i += 1
+      records[i].append(line[price_start:len(line)])
     else:
-      records[i].append(line)
-
-# NEXT:
-# check how long each sublist is
-# make each sublist the same length
-# line up like items in sublists
-# combine data that is spread accross multiple list items
-
-
+      for error in possible_errors:
+        if error in line:
+          records[i].append(line)
 
 with open('properties.csv', 'wb') as properties:
   writer = csv.writer(properties)
